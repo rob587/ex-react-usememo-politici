9a4e16212b6e3740f03c8bfb70ea209ff3fe10b8
rob587 <robertocamm02@yahoo.com>
2025-11-19 19:24:03 +0100

    seconda milestone completata

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-
 function App() {
   const url = "http://localhost:3333/politicians";
 
   const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     fetch(url)
@@ -14,19 +14,42 @@ function App() {
       });
   }, []);
 
+  const newArray =
+    data.filter((arr) =>
+      arr.name.toLowerCase().includes(inputValue.toLowerCase())
+    ) || arr.biography.toLowerCase().includes(inputValue.toLowerCase());
+
   return (
     <>
       <div className="container">
         <div className="row">
-          {data.map((d, i) => {
+          <div className="col-6">
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">
+                Ricerca Politici
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="inserisci politico"
+              />
+            </div>
+          </div>
+
+          {newArray.map((d, i) => {
             return (
               <>
-                <div className="card g-4" key={i}>
-                  <img src={d.image} alt={d.name} style={{ width: "10%" }} />
-                  <div className="container">
-                    <h4>{d.name}</h4>
-                    <p>{d.position}</p>
-                    <p>{d.biography}</p>
+                <div className="col-8">
+                  <div className="card g-4" key={i}>
+                    <img src={d.image} alt={d.name} style={{ width: "10%" }} />
+                    <div className="container">
+                      <h4>{d.name}</h4>
+                      <p>{d.position}</p>
+                      <p>{d.biography}</p>
+                    </div>
                   </div>
                 </div>
               </>
